ee3facf1c1d743f3f3cfef1516bab9551bf13be6
Anna Pierre-Louis <anitataksa3440@gmail.com>
2023-06-29 11:47:32 -0400

    REST OpenAPI3

---
+++ spring-19-OpenAPI3/src/test/java/com/cydeo/spring19openapi3/Spring19OpenApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring19openapi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring19OpenApi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
